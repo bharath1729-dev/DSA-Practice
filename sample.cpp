c8b3be95e3ff4f5cae57e61de1310cdb2498d8bd
bharath <bharath001230@gmail.com>
2026-04-20 11:32:02 +0530

    Added Leetcode 189: Rotate Array in C++

---
+++ sample.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float  a,b, h;
+printf("enter the height and breadth of the triangle\n");
+scanf(" %f %f",&b,&h);
+a = 0.5*b*h;
+printf("the area of the triangle is %f \n",a);
+return 0;
+
+
+}
